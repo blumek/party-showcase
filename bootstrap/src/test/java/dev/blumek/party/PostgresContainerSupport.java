bd7e4a31517d30caf3e4205f4d849af767b0e777
Szymon Blumczynski <szymon.blumczynski@gmail.com>
2026-06-27 10:12:48 +0200

    build: add postgres, spring-data-jdbc, flyway and testcontainers

---
+++ bootstrap/src/test/java/dev/blumek/party/PostgresContainerSupport.java
@@ -0,0 +1,16 @@
+package dev.blumek.party;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+class PostgresContainerSupport {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgres() {
+        return new PostgreSQLContainer("postgres:17-alpine");
+    }
+}
